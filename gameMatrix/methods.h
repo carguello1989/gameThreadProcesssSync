9dd738e061cc1cbe1666735d40e349c612e98b85
EstebanJCh97 <57113689+EstebanJCh97@users.noreply.github.com>
2022-09-19 10:31:09 -0600

    agregar cuartos al mapa
    los cuartos aun no tienen un camino definido para llegar al otro

--- gameMatrix/methods.h
+++ gameMatrix/methods.h
@@ -20,6 +20,7 @@ void fillMatrix(int dim, int matriz[dim][dim]){
 
 void doOperation(int dim, int matriz[dim][dim]){
   fillMatrix(dim,matriz);// esta parte llamo a la funcion
+  cuartos(dim,matriz);
   printf("\nLa matriz es la siguiente \n");
   showMatrix(dim,matriz);// esta parte llamo a la funcion para que me muestre la matriz
 }
@@ -28,6 +29,35 @@ int getNumberMonters(int dim){
   return dim/2;
 }
 
+// Se llena la matriz con la N cantidad de cuartos que estan representados con 1
+void cuartos(int dim, int matriz[dim][dim]){
+
+   int i,fila,columna,inicio,fin;
+   int contador = 0;
+   fila = rand()%dim;
+   columna = rand()%dim;
+   inicio = matriz[fila][columna];
+   matriz[fila][columna] = 1;
+   fila = rand()%dim;
+   columna = rand()%dim;
+   fin = matriz[fila][columna];
+   matriz[fila][columna] = 1;
+
+   for(i= 0;i<dim-2;i++){
+      fila = rand()%dim;
+      columna = rand()%dim;
+
+      if(matriz[fila][columna] == 0){   
+         matriz[fila][columna] = 1;
+         contador = contador+1;
+
+   }
+      else{
+         i = i-1;
+      }
+
+   }
+}
 
 void printMenu(){
   printf("Presione 1 para nivel facil:");
